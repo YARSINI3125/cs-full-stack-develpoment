b13ef1f522eaea88ffe0101ad391f154ea839218
YARSINI3125 <yarsinisri@gmail.com>
2025-02-18 11:22:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitDate() {
+    var name=document.getElementById('name').Value
+    var dob=document.getElementById('dob').Value
+    console.log(name+ "" +dob)
+    var output=document.getElementById('output');
+    output.innerText 
+}
